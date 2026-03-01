f687916abed3bc3d5abca038f605748422a119c7
xxy150310git <13385095693@163.com>
2026-03-01 11:00:32 +0800

    Add files via upload
    上传GESP OJ系统

---
+++ config.js
@@ -0,0 +1,19 @@
+window.ojConfig = {
+  "enabledLanguages": [
+    "C++"
+  ],
+  "enabledLevels": [
+    5,6
+  ],
+  "examStartTime": "2026-02-01 17:40:00",
+  "adminAccounts": [
+    {
+      "account": "xxy150310",
+      "pwd": "abcd1234"
+    },
+    {
+      "account": "Double_G",
+      "pwd": "123456"
+    }
+  ]
+};
