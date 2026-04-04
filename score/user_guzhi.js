a450f599106c71249f59dffb6589e6a4fafb8d0d
xxy150310git <13385095693@163.com>
2026-04-04 09:33:53 +0800

    Create user_guzhi.js

---
+++ score/user_guzhi.js
@@ -0,0 +1,93 @@
+window.userGuzhiConfig = [
+  {
+    "name": "Double_G",
+    "baseCredit": 95,
+    "practice": 0,
+    "achievement": 20,
+    "community": 80
+  },
+  {
+    "name": "chenqias",
+    "baseCredit": 80,
+    "practice": 0,
+    "achievement": 20,
+    "community": 40
+  },
+  {
+    "name": "wangyutang",
+    "baseCredit": 70,
+    "practice": 0,
+    "achievement": 15,
+    "community": 0
+  },
+  {
+    "name": "dashuaiguogai_xfy",
+    "baseCredit": 80,
+    "practice": 0,
+    "achievement": 20,
+    "community": 60
+  },
+  {
+    "name": "Lucky502",
+    "baseCredit": 100,
+    "practice": 0,
+    "achievement": 25,
+    "community": 15
+  },
+  {
+    "name": "cjk0526",
+    "baseCredit": 100,
+    "practice": 0,
+    "achievement": 0,
+    "community": 0
+  },
+  {
+    "name": "zby2025",
+    "baseCredit": 100,
+    "practice": 0,
+    "achievement": 0,
+    "community": 0
+  },
+  {
+    "name": "Houyc11",
+    "baseCredit": 100,
+    "practice": 0,
+    "achievement": 40,
+    "community": 0
+  },
+  {
+    "name": "8311wt5",
+    "baseCredit": 100,
+    "practice": 0,
+    "achievement": 10,
+    "community": 0
+  },
+  {
+    "name": "TT_chen",
+    "baseCredit": 100,
+    "practice": 0,
+    "achievement": 80,
+    "community": 95
+  },
+  {
+    "name": "CHINA_EASTERN_A350",
+    "baseCredit": 100,
+    "practice": 0,
+    "achievement": 0,
+    "community": 0
+  },
+  {
+    "name": "fjxmwth",
+    "baseCredit": 100,
+    "practice": 0,
+    "achievement": 0,
+    "community": 0
+  },
+  {
+    "name": "xxy150310",
+    "baseCredit": 100,
+    "practice": 100,
+    "achievement": 100,
+    "community": 100
+  }
+];
