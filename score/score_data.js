0dcaba92bfb37cdac899fd3bb2901021a99238d0
xxy150310git <13385095693@163.com>
2026-04-04 09:33:29 +0800

    Add score data for various matches

---
+++ score/score_data.js
@@ -0,0 +1,248 @@
+window.scoreData = [
+  {
+    "name": "Double_G",
+    "matchName": "abcd12345678 第一期入门赛",
+    "score": "11.1",
+    "time": "2025-12-28"
+  },
+  {
+    "name": "chenqias",
+    "matchName": "abcd12345678 第一期入门赛",
+    "score": "11.1",
+    "time": "2025-12-28"
+  },
+  {
+    "name": "wangyutang",
+    "matchName": "abcd12345678 第一期入门赛",
+    "score": "33.3",
+    "time": "2025-12-28"
+  },
+  {
+    "name": "dashuaiguogai_xfy",
+    "matchName": "abcd12345678 第一期入门赛",
+    "score": "77.8",
+    "time": "2025-12-28"
+  },
+  {
+    "name": "wangyutang",
+    "matchName": "二维数组",
+    "score": "16.7",
+    "time": "2025-12-27"
+  },
+  {
+    "name": "Lucky502",
+    "matchName": "CSP-J 初赛",
+    "score": "20",
+    "time": "2025-12-21"
+  },
+  {
+    "name": "dashuaiguogai_xfy",
+    "matchName": "CSP-X真题 - 上",
+    "score": "37.1",
+    "time": "2025-12-14"
+  },
+  {
+    "name": "cjk0526",
+    "matchName": "202601GESP 1级",
+    "score": "0",
+    "time": "2026-01-31"
+  },
+  {
+    "name": "zby2025",
+    "matchName": "202601GESP 4级",
+    "score": "0",
+    "time": "2026-01-31"
+  },
+  {
+    "name": "wangyutang",
+    "matchName": "202601GESP 4级",
+    "score": "18",
+    "time": "2026-01-31"
+  },
+  {
+    "name": "chenqias",
+    "matchName": "abcd12345678 第二期入门赛",
+    "score": "100",
+    "time": "2026-02-01"
+  },
+  {
+    "name": "wangyutang",
+    "matchName": "abcd12345678 第二期入门赛",
+    "score": "97.4",
+    "time": "2026-02-01"
+  },
+  {
+    "name": "Double_G",
+    "matchName": "abcd12345678 第二期入门赛",
+    "score": "44.4",
+    "time": "2026-02-01"
+  },
+  {
+    "name": "zby2025",
+    "matchName": "abcd12345678 第二期入门赛",
+    "score": "33.3",
+    "time": "2026-02-01"
+  },
+  {
+    "name": "wangyutang",
+    "matchName": "abcd12345678 第三期入门赛",
+    "score": "44.4",
+    "time": "2026-02-03"
+  },
+  {
+    "name": "chenqias",
+    "matchName": "abcd12345678 第三期入门赛",
+    "score": "33.3",
+    "time": "2026-02-03"
+  },
+  {
+    "name": "cjk0526",
+    "matchName": "abcd12345678 第三期入门赛",
+    "score": "33.3",
+    "time": "2026-02-03"
+  },
+  {
+    "name": "Double_G",
+    "matchName": "abcd12345678 第三期入门赛",
+    "score": "20.4",
+    "time": "2026-02-03"
+  },
+  {
+    "name": "dashuaiguogai_xfy",
+    "matchName": "abcd12345678 第三期入门赛",
+    "score": "11.1",
+    "time": "2026-02-03"
+  },
+  {
+    "name": "wangyutang",
+    "matchName": "202602 GESP 5级",
+    "score": "85.5",
+    "time": "2026-02-04"
+  },
+  {
+    "name": "zby2025",
+    "matchName": "202602 GESP 5级",
+    "score": "75",
+    "time": "2026-02-04"
+  },
+  {
+    "name": "cjk0526",
+    "matchName": "abcd12345678 第四期入门赛",
+    "score": "34",
+    "time": "2026-02-04"
+  },
+  {
+    "name": "Double_G",
+    "matchName": "abcd12345678 第四期入门赛",
+    "score": "20",
+    "time": "2026-02-04"
+  },
+  {
+    "name": "wangyutang",
+    "matchName": "abcd12345678 第二期基础赛",
+    "score": "45",
+    "time": "2026-02-04"
+  },
+  {
+    "name": "zby2025",
+    "matchName": "abcd12345678 第二期基础赛",
+    "score": "20",
+    "time": "2026-02-04"
+  },
+  {
+    "name": "zby2025",
+    "matchName": "abcd12345678 第三期基础赛",
+    "score": "25",
+    "time": "2026-02-06"
+  },
+  {
+    "name": "zby2025",
+    "matchName": "abcd12345678 第五期入门赛",
+    "score": "40",
+    "time": "2026-02-06"
+  },
+  {
+    "name": "Double_G",
+    "matchName": "abcd12345678 第五期入门赛",
+    "score": "60",
+    "time": "2026-02-06"
+  },
+  {
+    "name": "Houyc11",
+    "matchName": "abcd12345678 第四期基础赛",
+    "score": "100",
+    "time": "2026-02-07"
+  },
+  {
+    "name": "Houyc11",
+    "matchName": "abcd12345678 第七期入门赛",
+    "score": "100",
+    "time": "2026-03-01"
+  },
+  {
+    "name": "8311wt5",
+    "matchName": "abcd12345678 第八期入门赛",
+    "score": "79",
+    "time": "2026-03-22"
+  },
+  {
+    "name": "wangyutang",
+    "matchName": "abcd12345678 第八期入门赛",
+    "score": "37.5",
+    "time": "2026-03-22"
+  },
+  {
+    "name": "fjxmwth",
+    "matchName": "暂无评定比赛",
+    "score": "0",
+    "time": "0000-00-00"
+  },
+  {
+    "name": "TT_chen",
+    "matchName": "暂无评定比赛",
+    "score": "0",
+    "time": "0000-00-00"
+  },
+  {
+    "name": "CHINA_EASTERN_A350",
+    "matchName": "暂无评定比赛",
+    "score": "0",
+    "time": "0000-00-00"
+  },
+  {
+    "name": "xxy150310",
+    "matchName": "abcd12345678 第二期入门赛",
+    "score": "50",
+    "time": "2026-02-01"
+  },
+  {
+    "name": "xxy150310",
+    "matchName": "abcd12345678 第三期入门赛",
+    "score": "33.3",
+    "time": "2026-02-03"
+  },
+  {
+    "name": "xxy150310",
+    "matchName": "abcd12345678 第四期入门赛",
+    "score": "20",
+    "time": "2026-02-04"
+  },
+  {
+    "name": "xxy150310",
+    "matchName": "abcd12345678 第二期基础赛",
+    "score": "20",
+    "time": "2026-02-04"
+  },
+  {
+    "name": "xxy150310",
+    "matchName": "abcd12345678 第五期入门赛",
+    "score": "20",
+    "time": "2026-02-06"
+  },
+  {
+    "name": "xxy150310",
+    "matchName": "abcd12345678 第八期入门赛",
+    "score": "86.5",
+    "time": "2026-02-06"
+  },
+];
